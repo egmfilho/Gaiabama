67a83e8d18b4e71cb952e5ce9c2f51bbde4c6f07
egmfilho <edumig12@hotmail.com>
2017-11-17 11:47:13 -0200

    melhorias

--- app/scripts/constants/constants.js
+++ app/scripts/constants/constants.js
@@ -4,7 +4,6 @@ angular.module('alabama.constants')
   .constant('URLS', {
     // url do servidor, trocar aqui
     root: ''
-    // root: 'http://172.16.0.92/'
     // root: 'http://www.grupopaivaweb.com.br/'
     // root: 'http://tmp.gaia.adm.br/'
   });
--- gulpfile.js
+++ gulpfile.js
@@ -1,20 +1,26 @@
 'use strict';
 
-const gulp 	     = require('gulp');
-const gutil 	 = require('gulp-util');
-const connect    = require('gulp-connect');
-const rename     = require('gulp-rename');
-const pug 	     = require('gulp-pug');
-const sass       = require('gulp-sass');
-const uglify     = require('gulp-uglify');
-const sourcemaps = require('gulp-sourcemaps');
-const concat     = require('gulp-concat');
-const bower      = require('gulp-bower');
-const bowerFiles = require('main-bower-files');
-const cssnano    = require('gulp-cssnano');
+const gulp 	       = require('gulp');
+const gutil 	   = require('gulp-util');
+const connect      = require('gulp-connect');
+const rename       = require('gulp-rename');
+const pug 	       = require('gulp-pug');
+const sass         = require('gulp-sass');
+const uglify       = require('gulp-uglify');
+const sourcemaps   = require('gulp-sourcemaps');
+const concat       = require('gulp-concat');
+const bower        = require('gulp-bower');
+const bowerFiles   = require('main-bower-files');
+const cssnano      = require('gulp-cssnano');
+const rev          = require('gulp-rev');
+const collector    = require('gulp-rev-collector');
+const through      = require('through2');
+const gulpSequence = require('gulp-sequence');
+const fs           = require('fs');
+const path         = require('path');
 
-var source = 'app/'; // removed ./ due the undetection of new/deleted files
-var dest   = './www/';
+var source = 'app'; // removed ./ due the undetection of new/deleted files
+var dest   = 'www';
 
 function error_handler(error) {
 	// Output an error message
@@ -32,7 +38,7 @@ gulp.task('connect', function() {
 });
 
 gulp.task('index', function() {
-	return gulp.src(source + 'index.pug')
+	return gulp.src(path.join(source, 'index.pug'))
 		.pipe(pug({ }).on('error', error_handler))
 		.pipe(rename('index.php'))
 		.pipe(gulp.dest(dest))
@@ -40,23 +46,23 @@ gulp.task('index', function() {
 });
 
 gulp.task('compile-views', function() {
-	return gulp.src(source + 'views/*.pug')
+	return gulp.src(path.join(source, 'views/*.pug'))
 		.pipe(pug({ }).on('error', error_handler))
-		.pipe(gulp.dest(dest + 'views'))
+		.pipe(gulp.dest(path.join(dest, 'views')))
 		.pipe(connect.reload());
 });
 
 gulp.task('compile-partials', function() {
-	return gulp.src(source + 'partials/**/*.pug')
+	return gulp.src(path.join(source, 'partials/**/*.pug'))
 		.pipe(pug({ }).on('error', error_handler))
-		.pipe(gulp.dest(dest + 'partials'))
+		.pipe(gulp.dest(path.join(dest, 'partials')))
 		.pipe(connect.reload());
 });
 
 gulp.task('compile-sass', function() {
-	return gulp.src(source + 'styles/*.scss')
+	return gulp.src(path.join(source, 'styles/*.scss'))
 		.pipe(sass().on('error', error_handler))
-		.pipe(gulp.dest(dest + 'styles'))
+		.pipe(gulp.dest(path.join(dest, 'styles')))
 		.pipe(connect.reload());
 });
 
@@ -70,20 +76,20 @@ gulp.task('vendor-bundle', ['bower-restore'], function() {
 		.pipe(concat('vendors.min.js'))
 		.pipe(uglify())
 		.pipe(sourcemaps.write('maps/'))
-		.pipe(gulp.dest(dest + 'scripts'))
+		.pipe(gulp.dest(path.join(dest, 'scripts')))
 		.pipe(connect.reload());
 });
 
 gulp.task('app-bundle', function(cb) {
 	return gulp.src([
-			source + 'scripts/app.js',
-			source + 'scripts/**/*.js'
+			path.join(source, 'scripts/app.js'),
+			path.join(source, 'scripts/**/*.js')
 		])
 		.pipe(sourcemaps.init())
 		.pipe(concat('app.min.js'))
 		.pipe(uglify().on('error', error_handler))
 		.pipe(sourcemaps.write('maps/'))
-		.pipe(gulp.dest(dest + 'scripts'))
+		.pipe(gulp.dest(path.join(dest, 'scripts')))
 		.pipe(connect.reload());
 });
 
@@ -93,13 +99,13 @@ gulp.task("css", ["bower-restore"], function() {
 		.pipe(concat('vendor.min.css'))
 		.pipe(cssnano())
 		.pipe(sourcemaps.write('maps/'))
-		.pipe(gulp.dest(dest + 'styles'))
+		.pipe(gulp.dest(path.join(dest, 'styles')))
 		.pipe(connect.reload());
 });
 
 gulp.task('copy-images', function() {
-	return gulp.src(source + 'images/**/*.{png,gif,jpg,jpeg}')
-		.pipe(gulp.dest(dest + 'images'))
+	return gulp.src(path.join(source, 'images/**/*.{png,gif,jpg,jpeg}'))
+		.pipe(gulp.dest(path.join(dest, 'images')))
 		.pipe(connect.reload());
 });
 
@@ -107,34 +113,67 @@ gulp.task('copy-fonts', function() {
 	return gulp.src([
 			'./bower_components/bootstrap-sass/assets/fonts/**/*.*',
 			'./bower_components/font-awesome/fonts/**/*.*',
-			source + 'fonts/**/*.*'
+			path.join(source, 'fonts/**/*.*')
 		])
-		.pipe(gulp.dest(dest + 'fonts'))
+		.pipe(gulp.dest(path.join(dest, 'fonts')))
 		.pipe(connect.reload());
 });
 
 gulp.task('copy-videos', function() {	
-	return gulp.src(source + 'videos/**/*.{mp4,ogg,webm}')
-		.pipe(gulp.dest(dest + 'videos'))
+	return gulp.src(path.join(source, 'videos/**/*.{mp4,ogg,webm}'))
+		.pipe(gulp.dest(path.join(dest, 'videos')))
 		.pipe(connect.reload());
 });
 
+gulp.task('rev:rename', function() {
+	gulp.src([path.join(dest, 'partials/**/*.html'),
+			  path.join(dest, 'views/**/*.html'),
+			  path.join(dest, 'styles/**/*.css'),
+			  path.join(dest, 'scripts/**/*.js'),
+			  path.join(dest, 'images/**/*.{jpg,png,jpeg,gif,svg}')], { base: dest })
+		.pipe(rev())
+		.pipe(gulp.dest(dest))
+		.pipe(rev.manifest())
+		.pipe(gulp.dest(dest))
+		.pipe(through.obj(function(file, enc, callback) {
+			const manifest = JSON.parse(file.contents.toString());
+
+			for (var key in manifest) {
+				var filePath = path.join(__dirname, dest, key);
+				gutil.log(gutil.colors.red('DELETING'), filePath);
+				fs.unlinkSync(filePath, function(err) {
+					// TODO: emit an error if err
+				});
+			}
+			
+			this.push(file);
+			callback();
+		}));
+});
+
+gulp.task('rev:updateRefs', function() {
+	gulp.src([path.join(dest, '**/*.json'), path.join(dest, '**/*.{php,html,json,css,js}')])
+		.pipe(collector({
+			replaceReved: true
+		}))
+		.pipe(gulp.dest(dest));
+});
+
 gulp.task('watch', function() {
-	gulp.watch([source + '*.pug'], ['index']);
-	gulp.watch([source + 'views/*.pug'], ['compile-views']);
-	gulp.watch([source + 'partials/**/*.pug'], ['compile-partials']);
-	gulp.watch([source + 'templates/*.pug'], ['index', 'compile-views']);
-	gulp.watch([source + 'styles/*.scss'], ['compile-sass']);
-	gulp.watch([source + 'scripts/**/*.js'], ['app-bundle']);
+	gulp.watch([path.join(source, '*.pug')], ['index']);
+	gulp.watch([path.join(source, 'views/*.pug')], ['compile-views']);
+	gulp.watch([path.join(source, 'partials/**/*.pug')], ['compile-partials']);
+	gulp.watch([path.join(source, 'templates/*.pug')], ['index', 'compile-views']);
+	gulp.watch([path.join(source, 'styles/*.scss')], ['compile-sass']);
+	gulp.watch([path.join(source, 'scripts/**/*.js')], ['app-bundle']);
 	gulp.watch(bowerFiles({ filter: '**/*.css' }), ['css']);
 	gulp.watch(bowerFiles({ filter: '**/*.js' }), ['vendor-bundle']);
 	gulp.watch(bowerFiles(), ['copy-fonts']);
-	gulp.watch([source + 'images/**/*.*'], ['copy-images']);
-	gulp.watch([source + 'fonts/**/*.*'], ['copy-fonts']);
-	gulp.watch([source + 'videos/**/*.*'], ['copy-videos']);
+	gulp.watch([path.join(source, 'images/**/*.*')], ['copy-images']);
+	gulp.watch([path.join(source, 'fonts/**/*.*')], ['copy-fonts']);
+	gulp.watch([path.join(source, 'videos/**/*.*')], ['copy-videos']);
 });
 
 gulp.task('default', ['connect', 'watch']);
 
 gulp.task('build', ['index', 'compile-views', 'compile-partials', 'compile-sass', 'app-bundle', 'copy-images', 'vendor-bundle', 'css', 'copy-fonts', 'copy-videos']);
-
